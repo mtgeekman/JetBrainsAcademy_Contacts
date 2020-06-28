9c4fc8e6291623da217241ba32fc5e45927fd594
Michael Knudson <mtgeekman@gmail.com>
2020-06-28 15:24:20 -0700

    Finished Over loading section.

--- Contacts/Contacts/task/src/contacts/PhoneBook.java
+++ Contacts/Contacts/task/src/contacts/PhoneBook.java
@@ -1,5 +1,15 @@
 package contacts;
 
 public class PhoneBook {
+    public static void method(int i) { }
 
+    public static void method(int i, String s) { }
+
+    public static int method(int i) { return 3; }
+
+    public static void method(String s, int i) { }
+
+    public static long method(long l) { return 1L; }
+
+    public static void method1(int i) { }
 }
